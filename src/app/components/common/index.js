a439da3d6832c71be8e98dd59edb26f15c268788
Brown, David (US - Baltimore) <davidbrown3@deloitte.com>
2016-12-20 18:01:26 -0500

    fix for sample route and component

--- src/app/components/common/index.js
+++ src/app/components/common/index.js
@@ -2,10 +2,12 @@ import angular from 'angular';
 
 import {header} from './header/header';
 import {footer} from './footer/footer';
+import {tempComp} from './tempComp/tempComp';
 
 export const commonModule = 'common';
 
 angular
   .module(commonModule, [])
   .component('appHeader', header)
-  .component('appFooter', footer);
+  .component('appFooter', footer)
+  .component('tempComp', tempComp);
